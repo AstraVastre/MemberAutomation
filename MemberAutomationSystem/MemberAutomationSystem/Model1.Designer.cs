43732ef070fd245c1121de753f9d477d5a0dc427
astraxxx14 <xnnabsbsbsb@gmail.com>
2024-03-14 15:10:43 +0300

    Project
    Project

--- MemberAutomationSystem/MemberAutomationSystem/Model1.Designer.cs
+++ MemberAutomationSystem/MemberAutomationSystem/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\astra\source\repos\MemberAutomationSystem\MemberAutomationSystem\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\astra\OneDrive\Belgeler\GitHub\MemberAutomation\MemberAutomationSystem\MemberAutomationSystem\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- MemberAutomationSystem/MemberAutomationSystem/transactions.cs
+++ MemberAutomationSystem/MemberAutomationSystem/transactions.cs
@@ -7,5 +7,75 @@ namespace MemberAutomationSystem
 {
     public class transactions
     {
+        MemberAutomationEntities db = new MemberAutomationEntities();
+
+        public bool memberRegistration(string name, string surname, string TcNo, DateTime doBirth)
+        {
+            
+            try
+            {
+                
+                members members = new members();
+                members.name = name;
+                members.surname = surname;
+                members.Tc_No = TcNo;
+                members.date_Of_Birth = doBirth;
+                db.members.Add(members);
+                db.SaveChanges();
+                return true;
+
+            }
+            catch (Exception e) { return false; }
+        }
+
+        public bool memberControl(string TcNo)
+        {
+            try
+            {
+                members members = new members();
+                var member = db.members.SingleOrDefault(x => x.Tc_No == TcNo);
+                return member != null;
+            }
+            catch (Exception e) { return false; }
+
+        }
+
+        public bool memberInactive(int memberId)
+        {
+            try
+            {
+                var member = db.members.SingleOrDefault(x => x.member_Id == memberId);
+                if (member != null)
+                {
+                    member.is_Active = false;
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+
+            }
+            catch (Exception e) { return false; }
+        }
+
+        public bool memberUpdate(int memberId, string name, string surname, string TcNo, DateTime doBirth)
+        {
+            try
+            {
+                var member = db.members.SingleOrDefault(x => x.member_Id == memberId);
+                if (member != null)
+                {
+
+                    member.name = name;
+                    member.surname = surname;
+                    member.Tc_No = TcNo;
+                    member.date_Of_Birth = doBirth;
+                    db.SaveChanges();
+                    return true;
+
+                }
+                return false;
+            }
+            catch (Exception e) { return false; }
+        }
     }
 }
